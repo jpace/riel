18a65e20d49ed657ea172b0a8be7074ee9b25baf
Jeff Pace <jeugenepace@gmail.com>
2012-01-09 13:49:34 -0500

    Added file/dir processor.

--- lib/riel/fdprocessor.rb
+++ lib/riel/fdprocessor.rb
@@ -4,36 +4,39 @@
 require 'rubygems'
 require 'riel'
 
-# File and directory processor, with filtering.
+module RIEL
 
-class FileDirProcessor
-  def initialize args, filter = nil
-    @filter = filter
-    args.each do |arg|
-      process Pathname.new arg
+  # File and directory processor, with filtering.
+
+  class FileDirProcessor
+    def initialize args, filter = nil
+      @filter = filter
+      args.each do |arg|
+        process Pathname.new arg
+      end
     end
-  end
 
-  def process_file file
-  end
-
-  def process_directory dir
-    dir.children.sort.each do |fd|
-      next if @filter && @filter.include?(fd.basename.to_s)
-      process fd
+    def process_file file
     end
-  end
 
-  def process fd
-    if fd.directory?
-      process_directory fd
-    elsif fd.file?
-      process_file fd
-    else
-      process_unknown_type fd
+    def process_directory dir
+      dir.children.sort.each do |fd|
+        next if @filter && @filter.include?(fd.basename.to_s)
+        process fd
+      end
     end
-  end
 
-  def process_unknown_type fd
+    def process fd
+      if fd.directory?
+        process_directory fd
+      elsif fd.file?
+        process_file fd
+      else
+        process_unknown_type fd
+      end
+    end
+
+    def process_unknown_type fd
+    end
   end
 end
