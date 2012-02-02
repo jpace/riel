0ac7c5c8ef77f13bd454c7871ef277677e2c1ce4
Jeff Pace <jeugenepace@gmail.com>
2012-02-02 07:37:43 -0500

    Added total and averaging behavior to Table class.

--- lib/riel/asciitable/table.rb
+++ lib/riel/asciitable/table.rb
@@ -8,6 +8,17 @@ require 'riel/asciitable/row'
 
 module RIEL
   module ASCIITable
+    class TableData
+      def keys
+      end
+
+      def fields
+      end
+      
+      def value key, field
+      end
+    end
+
     class Table
       include Loggable      
 
@@ -18,6 +29,35 @@ module RIEL
         @columns = Array.new
         @separator_rows = Hash.new
         @default_value = args[:default_value] || ""
+
+        set_headings
+        set_cells
+
+        if sepival = args[:separators]
+          add_separators sepival
+        end
+
+        totrow = args[:has_total_row]
+        avgrow = args[:has_average_row]
+
+        if totrow || avgrow
+          add_separator_row '='
+          if totrow
+            TotalRow.new(self)
+          end
+          
+          if avgrow
+            AverageRow.new(self)
+          end
+        end
+        
+        if args[:has_total_columns]
+          add_total_columns
+        end
+        
+        if args[:highlight_max_cells]
+          highlight_max_cells
+        end
       end
 
       # sets a separator for the row preceding +rownum+. Does not change the
@@ -126,6 +166,10 @@ module RIEL
         cells.collect { |cell| cell.value }.uniq.sort.reverse
       end
 
+      def data_cells_for_row row, offset
+        cells_for_row row, offset, fields.length * data_cell_span
+      end
+
       def get_highlight_colors 
         Array.new
       end
@@ -155,8 +199,9 @@ module RIEL
 
       def highlight_max_cells
         (1 .. last_row).each do |row|
-          highlight_cells_in_row row, 0
-          highlight_cells_in_row row, 1
+          (0 .. (data_cell_span - 1)).each do |offset|
+            highlight_cells_in_row row, offset
+          end
         end
 
         0.upto(1) do |n|
@@ -164,6 +209,87 @@ module RIEL
         end
       end
 
+      def data_cell_span
+        1
+      end
+
+      def set_headings
+        cellspan = data_cell_span
+
+        colidx = 0
+        set colidx, 0, headings[0]
+
+        colidx += 1
+
+        (1 ... headings.length).each do |hi|
+          set colidx, 0, headings[hi]
+          if cellspan > 1
+            set_cellspan colidx, colidx - 1 + cellspan, 0
+          end
+          colidx += cellspan
+        end
+      end
+
+      def add_total_columns
+        last_data_col = data_columns.last
+        totcol = last_data_col + 1
+
+        (1 .. last_row).each do |row|
+          (0 .. (data_cell_span - 1)).each do |offset|
+            rowcells = cells_for_row row, offset, last_data_col
+            total = rowcells.collect { |cell| cell.value }.inject(0) { |sum, num| sum + num }
+            set totcol + offset, row, total
+          end
+        end
+      end
+
+      # returns the cells up to the given column
+      def cells_for_row row, offset, maxcol
+        cells = cells_in_row row
+        cells = cells[1 .. maxcol]
+        cells.select_with_index { |cell, cidx| (cidx % data_cell_span) == offset }
+      end    
+
+      def set_cells
+        rownum = 1
+        keys.each_with_index do |name, nidx|
+          set 0, rownum, name
+          
+          colidx = 1
+          fields.each do |field|
+            (0 .. (data_cell_span - 1)).each do |didx|
+              val = value name, field, didx
+              set colidx, rownum, val
+              colidx += 1
+            end
+          end
+          rownum += 1
+        end
+      end
+
+      def data
+        nil
+      end
+
+      def keys
+        data.keys
+      end
+
+      def fields
+        data.fields
+      end
+
+      def value key, field, index
+        data.value key, field, index
+      end
+
+      def data_rows
+        (1 .. keys.length)
+      end
+
+      def data_columns
+        (1 .. fields.length * data_cell_span)
+      end
     end
   end
 end
