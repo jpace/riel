4a7e3a166d756a9a3ef49c5df40917e67c89e641
Jeff Pace <jeugenepace@gmail.com>
2013-03-07 19:36:41 -0500

    Removed dependency on RIEL::String.

--- lib/riel/pathname.rb
+++ lib/riel/pathname.rb
@@ -9,7 +9,7 @@ class Pathname
   # a compliment to the +dirname+, +basename+, and +extname+ family, this returns
   # the basename without the extension, e.g. "foo" from "/usr/share/lib/foo.bar".
   def rootname
-    basename.to_s - extname.to_s
+    basename.to_s.sub extname.to_s, ''
   end
 
   # Returns an array of the path split into its components.
